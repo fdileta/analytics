384eae46c9755135fdbf6f2263e6d83d02b79ed7
Michael Walker <mwalker@gitlab.com>
2020-08-21 16:23:35 -0500

    add surrogate key for project_statistics_snapshot to determine what has changed

--- transform/snowflake-dbt/snapshots/gitlab_dotcom/gitlab_dotcom_project_statistics_snapshots.sql
+++ transform/snowflake-dbt/snapshots/gitlab_dotcom/gitlab_dotcom_project_statistics_snapshots.sql
@@ -4,7 +4,12 @@
         config(
           unique_key='id',
           strategy='check',
-          check_cols=[
+          check_cols=['snapshot_surrogate_key'],
+        )
+    }}
+    
+    SELECT *,
+      {{ dbt_utils.surrogate_key([
               'repository_size', 
               'commit_count', 
               'storage_size', 
@@ -15,11 +20,7 @@
               'build_artifacts_size', 
               'shared_runners_seconds', 
               'shared_runners_seconds_last_reset',
-          ],
-        )
-    }}
-    
-    SELECT *       
+          ]) }} AS snapshot_surrogate_key
     FROM {{ source('gitlab_dotcom', 'project_statistics') }}
     QUALIFY ROW_NUMBER() OVER (PARTITION BY id ORDER BY _uploaded_at DESC) = 1
     
