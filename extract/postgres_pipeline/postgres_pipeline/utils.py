2b2fae7a66ae9401df8ab1727cf3a4dc32f1c7a5
parmstrong <parmstrong@gitlab.com>
2020-08-30 23:12:05 +0200

    Testing stuff

--- extract/postgres_pipeline/postgres_pipeline/utils.py
+++ extract/postgres_pipeline/postgres_pipeline/utils.py
@@ -193,7 +193,7 @@ def chunk_and_upload(
     rows_uploaded = 0
 
     type_df = query_results_generator(query, source_engine)
-    logging.info(type_df.next().info())
+    logging.info(type_df.__next__().info())
 
     with tempfile.TemporaryFile() as tmpfile:
         iter_csv = read_sql_tmpfile(query, source_engine, tmpfile)
