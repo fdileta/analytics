a29ac637ff95871f8c433823ea7afbf22d4cb9d7
Melia Vilain <mvilain@gitlab.com>
2020-12-02 00:41:28 +0000

    Update type of arr change to use 180 day rule

--- transform/snowflake-dbt/macros/marts/arr/type_of_arr_change.sql
+++ transform/snowflake-dbt/macros/marts/arr/type_of_arr_change.sql
@@ -1,13 +1,13 @@
 {%- macro type_of_arr_change(arr, previous_arr) -%}
 
    CASE
-     WHEN {{ previous_arr }} = 0 AND {{ arr }} > 0
+     WHEN {{ previous_arr }} = 0 AND {{ arr }} > 0 AND {{ days_between_arr }} IS NULL OR {{ days_between_arr }} >= 180
        THEN 'New'
      WHEN {{ arr }} = 0 AND {{ previous_arr }} > 0
        THEN 'Churn'
      WHEN {{ arr }} < {{ previous_arr }} AND {{ arr }} > 0
        THEN 'Contraction'
-     WHEN {{ arr }} > {{ previous_arr }}
+     WHEN {{ arr }} > {{ previous_arr }} AND {{ days_between_arr }} < 180
        THEN 'Expansion'
      WHEN {{ arr }} = {{ previous_arr }}
        THEN 'No Impact'
