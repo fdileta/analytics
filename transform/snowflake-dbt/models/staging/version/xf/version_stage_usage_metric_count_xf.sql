c2565a5ac5a5581099deadefbf2192371e8c65a0
Kathleen Tam <ktam@gitlab.com>
2020-12-07 17:37:53 +0000

    Adding new data model for demo purposes

---
+++ transform/snowflake-dbt/models/staging/version/xf/version_stage_usage_metric_count_xf.sql
@@ -0,0 +1,57 @@
+WITH usage_data AS (
+
+    SELECT *
+    FROM {{ ref('version_usage_data_unpacked_intermediate') }}
+
+), unpacked_stage_json AS (
+
+    SELECT
+      usage_data.*,
+      f.key                                                              AS stage_name,
+      f.value                                                            AS stage_activity_count_json
+    FROM usage_data,
+      lateral flatten(input => usage_data.usage_activity_by_stage_monthly) f
+    WHERE IS_OBJECT(f.value) = TRUE
+    
+    UNION 
+
+    SELECT
+      usage_data.*,
+      'manage'                                                           AS stage_name, 
+      analytics_unique_visits                                            AS stage_activity_count_json   
+    FROM usage_data
+
+), final AS (
+
+    SELECT
+      unpacked_stage_json.id,
+      unpacked_stage_json.version,
+      unpacked_stage_json.created_at,
+      --unpacked_stage_json.license_id,
+      unpacked_stage_json.uuid,
+      unpacked_stage_json.edition,      
+      unpacked_stage_json.ping_source,
+      unpacked_stage_json.major_version,
+      unpacked_stage_json.main_edition,
+      unpacked_stage_json.edition_type,
+      unpacked_stage_json.license_plan_code,
+      unpacked_stage_json.company,
+      unpacked_stage_json.zuora_subscription_id,
+      unpacked_stage_json.zuora_subscription_status,
+      unpacked_stage_json.zuora_crm_id,
+      unpacked_stage_json.stage_name,
+      DATEADD('days', -28, unpacked_stage_json.created_at) AS period_start,
+      unpacked_stage_json.created_at                       AS period_end,
+      f.key                                                AS usage_action_name,
+      f.value                                              AS usage_action_count
+    FROM unpacked_stage_json,
+      lateral flatten(input => unpacked_stage_json.stage_activity_count_json) f
+
+)
+
+SELECT DISTINCT 
+  stage_name,
+  usage_action_name, 
+  usage_action_count
+FROM final 
+ORDER BY 1,2
