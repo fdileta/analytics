4ff26c6012377d7067f48b7fad63db4850bbb994
parmstrong <parmstrong@gitlab.com>
2021-01-07 20:31:16 +0200

    first draft fct_mrr_totals_levelled.sql

--- transform/snowflake-dbt/models/common/fct_mrr_totals_levelled.sql
+++ transform/snowflake-dbt/models/common/fct_mrr_totals_levelled.sql
@@ -1,47 +1,16 @@
+WITH sfdc_accounts_xf AS (
 
-with zuora_mrr_totals AS (
+      SELECT * FROM {{ref('sfdc_accounts_xf')}}
 
-    SELECT * FROM {{ref('zuora_mrr_totals')}}
+), bridge_table AS (
 
-), zuora_account AS (
+      SELECT * FROM {{ ref('bdg_account_levels ') }}
 
-    SELECT * FROM {{ref('zuora_account')}}
-
-), sfdc_accounts_xf AS (
-
-    SELECT * FROM {{ref('sfdc_accounts_xf')}}
-
-), sfdc_deleted_accounts AS (
-
-    SELECT * FROM {{ref('sfdc_deleted_accounts')}}
-
-), initial_join_to_sfdc AS (
+), joined AS (
 
     SELECT
-           zuora_mrr_totals.*,
-           zuora_account.account_id AS zuora_account_id,
-           zuora_account.account_name AS zuora_account_name,
-           zuora_account.crm_id as zuora_crm_id,
-           sfdc_accounts_xf.account_id as sfdc_account_id_int
-    FROM zuora_mrr_totals
-    LEFT JOIN zuora_account
-    ON zuora_account.account_number = zuora_mrr_totals.account_number
-    LEFT JOIN sfdc_accounts_xf
-    ON sfdc_accounts_xf.account_id = zuora_account.crm_id
-
-), replace_sfdc_account_id_with_master_record_id AS (
-
-    SELECT coalesce(initial_join_to_sfdc.sfdc_account_id_int, sfdc_master_record_id) AS sfdc_account_id,
-          initial_join_to_sfdc.*
-    FROM initial_join_to_sfdc
-    LEFT JOIN sfdc_deleted_accounts
-    ON initial_join_to_sfdc.zuora_crm_id = sfdc_deleted_accounts.sfdc_account_id
-
-), joined as (
-
-    SELECT
-           replace_sfdc_account_id_with_master_record_id.country   AS zuora_sold_to_country,
-           replace_sfdc_account_id_with_master_record_id.account_number   AS zuora_account_number,
+           replace_sfdc_account_id_with_master_record_id.country                            AS zuora_sold_to_country,
+           replace_sfdc_account_id_with_master_record_id.account_number                     AS zuora_account_number,
            replace_sfdc_account_id_with_master_record_id.subscription_name_slugify,
            replace_sfdc_account_id_with_master_record_id.subscription_name,
            replace_sfdc_account_id_with_master_record_id.oldest_subscription_in_cohort,
@@ -80,13 +49,24 @@ with zuora_mrr_totals AS (
     LEFT JOIN sfdc_accounts_xf
     ON sfdc_accounts_xf.account_id = replace_sfdc_account_id_with_master_record_id.sfdc_account_id
 
+), mrr_totals_levelled AS (
+
+    SELECT *,
+           datediff(month, zuora_account_cohort_month, mrr_month)      as months_since_zuora_account_cohort_start,
+           datediff(quarter, zuora_account_cohort_quarter, mrr_month)  as quarters_since_zuora_account_cohort_start,
+           datediff(month, sfdc_account_cohort_month, mrr_month)       as months_since_sfdc_account_cohort_start,
+           datediff(quarter, sfdc_account_cohort_quarter, mrr_month)   as quarters_since_sfdc_account_cohort_start,
+           datediff(month, parent_account_cohort_month, mrr_month)     as months_since_parent_account_cohort_start,
+           datediff(quarter, parent_account_cohort_quarter, mrr_month) as quarters_since_parent_account_cohort_start
+    FROM joined
+
 )
 
-SELECT *,
-      datediff(month, zuora_account_cohort_month, mrr_month) as months_since_zuora_account_cohort_start,
-      datediff(quarter, zuora_account_cohort_quarter, mrr_month) as quarters_since_zuora_account_cohort_start,
-      datediff(month, sfdc_account_cohort_month, mrr_month) as months_since_sfdc_account_cohort_start,
-      datediff(quarter, sfdc_account_cohort_quarter, mrr_month) as quarters_since_sfdc_account_cohort_start,
-      datediff(month, parent_account_cohort_month, mrr_month) as months_since_parent_account_cohort_start,
-      datediff(quarter, parent_account_cohort_quarter, mrr_month) as quarters_since_parent_account_cohort_start
-FROM joined
+
+{{ dbt_audit(
+    cte_ref="mrr_totals_levelled",
+    created_by="@paul_armstrong",
+    updated_by="@paul_armstrong",
+    created_date="2021-01-07",
+    updated_date="2021-01-07"
+) }}
