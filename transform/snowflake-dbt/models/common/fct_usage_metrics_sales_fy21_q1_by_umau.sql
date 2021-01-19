80019bbff6099c1c57487b72602f8c0cc257ace5
Kathleen Tam <ktam@gitlab.com>
2021-01-19 01:01:56 +0000

    Add three fact tables for the usage metrics requested by the sales team

---
+++ transform/snowflake-dbt/models/common/fct_usage_metrics_sales_fy21_q1_by_umau.sql
@@ -0,0 +1,29 @@
+WITH prep_usage_ping_subscription_mapped_sales_fy21_q1 AS (
+  
+    SELECT *  
+    FROM {{ ref('prep_usage_ping_subscription_mapped_sales_fy21_q1') }}
+
+), cleaned AS (
+  
+    SELECT 
+      *, 
+      LAST_VALUE(dim_usage_ping_id) OVER (PARTITION BY subscription_id, ping_created_at_month ORDER BY dim_usage_ping_id) AS latest_usage_ping_id_in_month
+    FROM prep_usage_ping_subscription_mapped_sales_fy21_q1
+    WHERE subscription_id IS NOT NULL 
+  
+), final AS (
+
+    SELECT *
+    FROM cleaned
+    QUALIFY ROW_NUMBER() over (PARTITION BY subscription_id, ping_created_at_month ORDER BY UMAU_28_DAYS_USER DESC) = 1
+
+)
+
+{{ dbt_audit(
+    cte_ref="final",
+    created_by="@kathleentam",
+    updated_by="@kathleentam",
+    created_date="2021-01-18",
+    updated_date="2021-01-18"
+) }}
+
