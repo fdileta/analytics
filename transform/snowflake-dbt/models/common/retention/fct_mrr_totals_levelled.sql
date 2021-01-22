2ce78268372c39748126d9a4c86fd07a75fe7767
parmstrong <parmstrong@gitlab.com>
2021-01-22 13:43:51 +0200

    Corrected cases, added in ultimate parent_id

--- transform/snowflake-dbt/models/common/retention/fct_mrr_totals_levelled.sql
+++ transform/snowflake-dbt/models/common/retention/fct_mrr_totals_levelled.sql
@@ -14,28 +14,29 @@ WITH mrr_totals AS (
 
     SELECT
            mrr_totals.*,
-           billing_account.DIM_BILLING_ACCOUNT_ID AS zuora_account_id,
-           billing_account.BILLING_ACCOUNT_NAME AS zuora_account_name,
-           billing_account.BILLING_ACCOUNT_NAME as zuora_crm_id,
-           crm_account.CRM_ACCOUNT_ID as sfdc_account_id,
-           crm_account.ULTIMATE_PARENT_ACCOUNT_ID as ULTIMATE_PARENT_ACCOUNT_ID,
+           billing_account.dim_billing_account_id                           AS zuora_account_id,
+           billing_account.billing_account_name                             AS zuora_account_name,
+           billing_account.billing_account_name                             AS zuora_crm_id,
+           crm_account.crm_account_id                                       AS sfdc_account_id,
+           crm_account.ultimate_parent_account_id                           AS ultimate_parent_account_id,
+           crm_account.ultimate_parent_account_name                         AS ultimate_parent_account_name,
            min(zuora_subscription_cohort_month) OVER (
               PARTITION BY zuora_account_id)                                AS zuora_account_cohort_month,
            min(zuora_subscription_cohort_quarter) OVER (
               PARTITION BY zuora_account_id)                                AS zuora_account_cohort_quarter,
            min(zuora_subscription_cohort_month) OVER (
-              PARTITION BY sfdc_account_id)                     AS sfdc_account_cohort_month,
+              PARTITION BY sfdc_account_id)                                 AS sfdc_account_cohort_month,
            min(zuora_subscription_cohort_quarter) OVER (
-              PARTITION BY sfdc_account_id)                     AS sfdc_account_cohort_quarter,
+              PARTITION BY sfdc_account_id)                                 AS sfdc_account_cohort_quarter,
            min(zuora_subscription_cohort_month) OVER (
               PARTITION BY ultimate_parent_account_id)                      AS parent_account_cohort_month,
            min(zuora_subscription_cohort_quarter) OVER (
               PARTITION BY ultimate_parent_account_id)                      AS parent_account_cohort_quarter
     FROM mrr_totals
     LEFT JOIN billing_account
-    ON billing_account.BILLING_ACCOUNT_NUMBER = mrr_totals.ACCOUNT_NUMBER
+    ON billing_account.billing_account_number = mrr_totals.account_number
     LEFT JOIN crm_account
-    ON crm_account.crm_account_id = billing_account.DIM_CRM_ACCOUNT_ID
+    ON crm_account.crm_account_id = billing_account.dim_crm_account_id
 
 ), final_table AS (
 
