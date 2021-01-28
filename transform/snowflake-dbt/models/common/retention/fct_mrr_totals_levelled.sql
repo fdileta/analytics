0ffc06354ede8141d959ab0ac171f1a3b85fa7a1
Paul Armstrong <parmstrong@gitlab.com>
2021-01-28 23:29:13 +0200

    Corrected join, select from subscription

--- transform/snowflake-dbt/models/common/retention/fct_mrr_totals_levelled.sql
+++ transform/snowflake-dbt/models/common/retention/fct_mrr_totals_levelled.sql
@@ -1,6 +1,10 @@
 WITH mrr_totals AS (
 
-    SELECT * FROM {{ ref('zuora_mrr_totals') }}
+    SELECT * FROM {{ ref('fct_mrr') }}
+
+), subscription AS (
+
+    SELECT * FROM {{ ref('dim_subscription') }}
 
 ), billing_account AS (
 
@@ -12,38 +16,39 @@ WITH mrr_totals AS (
 
 ), joined AS (
 
-    SELECT
+     SELECT
            mrr_totals.*,
-           billing_account.dim_billing_account_id                           AS zuora_account_id,
-           billing_account.billing_account_name                             AS zuora_account_name,
-           billing_account.billing_account_name                             AS zuora_crm_id,
+           billing_account.dim_billing_account_id                           AS billing_account_id,
+           billing_account.billing_account_name                             AS billing_account_name,
+           billing_account.billing_account_name                             AS billing_crm_id,
            crm_account.crm_account_id                                       AS sfdc_account_id,
            crm_account.crm_account_name                                     AS sfdc_account_name,
            crm_account.ultimate_parent_account_id                           AS ultimate_parent_account_id,
            crm_account.ultimate_parent_account_name                         AS ultimate_parent_account_name,
-           min(zuora_subscription_cohort_month) OVER (
-              PARTITION BY zuora_account_id)                                AS zuora_account_cohort_month,
-           min(zuora_subscription_cohort_quarter) OVER (
-              PARTITION BY zuora_account_id)                                AS zuora_account_cohort_quarter,
-           min(zuora_subscription_cohort_month) OVER (
-              PARTITION BY sfdc_account_id)                                 AS sfdc_account_cohort_month,
-           min(zuora_subscription_cohort_quarter) OVER (
-              PARTITION BY sfdc_account_id)                                 AS sfdc_account_cohort_quarter,
-           min(zuora_subscription_cohort_month) OVER (
-              PARTITION BY ultimate_parent_account_id)                      AS parent_account_cohort_month,
-           min(zuora_subscription_cohort_quarter) OVER (
-              PARTITION BY ultimate_parent_account_id)                      AS parent_account_cohort_quarter
+           min(subscription.cohort_month) OVER (
+              PARTITION BY billing_account.dim_billing_account_id)          AS billing_account_cohort_month,
+           min(subscription.cohort_quarter) OVER (
+              PARTITION BY billing_account.dim_billing_account_id)          AS billing_account_cohort_quarter,
+           min(subscription.cohort_month) OVER (
+              PARTITION BY crm_account.crm_account_id)                      AS sfdc_account_cohort_month,
+           min(subscription.cohort_quarter) OVER (
+              PARTITION BY crm_account.crm_account_id)                      AS sfdc_account_cohort_quarter,
+           min(subscription.cohort_month) OVER (
+              PARTITION BY crm_account.ultimate_parent_account_id)          AS parent_account_cohort_month,
+           min(subscription.cohort_quarter) OVER (
+              PARTITION BY crm_account.ultimate_parent_account_id)          AS parent_account_cohort_quarter
     FROM mrr_totals
     LEFT JOIN billing_account
-    ON billing_account.billing_account_number = mrr_totals.account_number
+    ON billing_account.dim_billing_account_id = mrr_totals.dim_billing_account_id
     LEFT JOIN crm_account
     ON crm_account.crm_account_id = billing_account.dim_crm_account_id
+    LEFT JOIN subscription ON subscription.dim_subscription_id = mrr_totals.dim_subscription_id
 
 ), final_table AS (
 
    SELECT *,
-      datediff(month, zuora_account_cohort_month, mrr_month) as months_since_zuora_account_cohort_start,
-      datediff(quarter, zuora_account_cohort_quarter, mrr_month) as quarters_since_zuora_account_cohort_start,
+      datediff(month, billing_account_cohort_month, mrr_month) as months_since_billing_account_cohort_start,
+      datediff(quarter, billing_account_cohort_quarter, mrr_month) as quarters_since_billing_account_cohort_start,
       datediff(month, sfdc_account_cohort_month, mrr_month) as months_since_sfdc_account_cohort_start,
       datediff(quarter, sfdc_account_cohort_quarter, mrr_month) as quarters_since_sfdc_account_cohort_start,
       datediff(month, parent_account_cohort_month, mrr_month) as months_since_parent_account_cohort_start,
