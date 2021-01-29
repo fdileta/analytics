9a85fc5e4c41ed2d8fee726602e8f9d757660423
Paul Armstrong <parmstrong@gitlab.com>
2021-01-29 13:09:48 +0200

    Changed to using full words in joins instead of aliases

--- transform/snowflake-dbt/models/common/dim_subscription.sql
+++ transform/snowflake-dbt/models/common/dim_subscription.sql
@@ -12,44 +12,44 @@ WITH subscription AS (
 
   SELECT
     --ids & keys
-    s.dim_subscription_id,
-    s.dim_crm_account_id,
-    s.dim_billing_account_id,
-    s.dim_crm_person_id_invoice_owner,
-    s.dim_crm_opportunity_id,
-    s.dim_subscription_id_original,
-    s.dim_subscription_id_previous,
-    s.amendment_id,
+    subscription.dim_subscription_id,
+    subscription.dim_crm_account_id,
+    subscription.dim_billing_account_id,
+    subscription.dim_crm_person_id_invoice_owner,
+    subscription.dim_crm_opportunity_id,
+    subscription.dim_subscription_id_original,
+    subscription.dim_subscription_id_previous,
+    subscription.amendment_id,
 
     --info
-    s.subscription_name,
-    s.subscription_name_slugify,
-    s.subscription_status,
-    s.subscription_version,
-    s.is_auto_renew,
-    s.zuora_renewal_subscription_name,
-    s.zuora_renewal_subscription_name_slugify,
-    s.renewal_term,
-    s.renewal_term_period_type,
-    s.subscription_start_date,
-    s.subscription_end_date,
-    s.subscription_sales_type,
-    s.subscription_start_month,
-    s.subscription_end_month,
-    sl.lineage,
-    sl.oldest_subscription_in_cohort,
-    sl.cohort_month,
-    sl.cohort_quarter,
-    sl.cohort_year
-  FROM subscription s
-  LEFT JOIN subscription_lineage sl on sl.dim_subscription_id = s.dim_subscription_id
+    subscription.subscription_name,
+    subscription.subscription_name_slugify,
+    subscription.subscription_status,
+    subscription.subscription_version,
+    subscription.is_auto_renew,
+    subscription.zuora_renewal_subscription_name,
+    subscription.zuora_renewal_subscription_name_slugify,
+    subscription.renewal_term,
+    subscription.renewal_term_period_type,
+    subscription.subscription_start_date,
+    subscription.subscription_end_date,
+    subscription.subscription_sales_type,
+    subscription.subscription_start_month,
+    subscription.subscription_end_month,
+    subscription_lineage.lineage,
+    subscription_lineage.oldest_subscription_in_cohort,
+    subscription_lineage.cohort_month,
+    subscription_lineage.cohort_quarter,
+    subscription_lineage.cohort_year
+  FROM subscription
+  LEFT JOIN subscription_lineage ON subscription_lineage.dim_subscription_id = subscription.dim_subscription_id
 
 )
 
 {{ dbt_audit(
     cte_ref="final",
     created_by="@snalamaru",
-    updated_by="@ischweickartDD",
+    updated_by="@paul_armstrong",
     created_date="2020-12-16",
-    updated_date="2021-01-07"
+    updated_date="2021-01-29"
 ) }}
